d0e86b18c2606299f4fb8192a9ec9559b49486a8
mschwartze <mschwartze@gmail.com>
2024-01-31 16:31:10 -0500

    moved sample JSON from rails project

---
+++ _OLD/metricUpdateSampleData.js
@@ -0,0 +1,19 @@
+// sample of JSON sent to API
+sampleData = {
+    "metric_update": {
+      "instagram_id": "C1cy_05p5fZ",
+      "recorded_at": "2024-01-01",
+      "update_collection": {
+        "average_watch_time_in_seconds": 5,
+        "watch_time_in_seconds": 100,
+        "initial_plays_count": 10,
+        "replays_count": 5,
+        "likes_count": 14,
+        "comments_count": 10,
+        "shares_count": 2,
+        "saves_count": 0,
+        "follower_accounts_reached_count": 2,
+        "non_follower_accounts_reached_count": 3
+      }
+    }
+  }
